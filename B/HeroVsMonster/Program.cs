a1fdcb1ba09c07b432d885b117e42d5e7dca32f3
Vladimir Shnyakin <shadowflown@gmail.com>
2022-04-06 09:47:38 -0400

    Who will win?

---
+++ B/HeroVsMonster/Program.cs
@@ -0,0 +1,30 @@
+﻿int hero = 10;
+int monster = 10;
+Random attack = new Random();
+int hit = 0;
+
+while ((hero > 0) || (monster > 0))
+{   
+    if ((hero > 0) && (monster > 0))
+    {
+        hit = attack.Next(1, 11);
+        monster -= hit;
+        Console.WriteLine($"Monster was damaged and lost {hit} health and now has {monster} health.");
+    }
+    else 
+    {
+        Console.WriteLine("Monster wins!");
+        break;
+    }
+    if ((hero > 0) && (monster > 0))
+    {
+        hit = attack.Next(1, 11);
+        hero -= hit;
+        Console.WriteLine($"Hero was damaged and lost {hit} health and now has {hero} health.");
+    }
+    else 
+    {
+        Console.WriteLine("Hero wins!");
+        break;  
+    }       
+} 
