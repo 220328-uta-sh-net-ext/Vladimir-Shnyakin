8ada81c584df9fea065698be2ccdbe5c6555fa6a
Vladimir Shnyakin <shadowflown@gmail.com>
2022-05-14 18:35:24 -0400

    Finished sanple

--- ConsoleAppToConsumeAPI/ConsoleAppToConsumeAPI/Program.cs
+++ ConsoleAppToConsumeAPI/ConsoleAppToConsumeAPI/Program.cs
@@ -1,42 +1,94 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text.Json;
-using System.Threading.Tasks;
-
-namespace WebAPIClient
+﻿namespace HttpClientSample
 {
+    public class Review
+    {
+        public string RestaurantName { get; set; }
+        public double StarsTaste { get; set; }
+        public double StarsMood { get; set; }
+        public double StarsService { get; set; }
+        public double StarsPrice { get; set; }
+        public string Note { get; set; }
+        public string UserName { get; set; }
+    }
+        public class Restaurant
+    {
+        public string RestaurantName { get; set; }
+        public string Cuisine { get; set; }
+        public double OverallRating { get; set; }
+        public List<Review> Reviews { get; set; }
+    }
+
     class Program
     {
-        private static readonly HttpClient client = new HttpClient();
+        static HttpClient client = new HttpClient();
 
-        static async Task Main(string[] args)
+        static void ShowRestaurant(Restaurant restaurant)
         {
-            var repositories = await ProcessRepositories();
-
-            foreach (var repo in repositories)
+            Console.WriteLine($"\"{restaurant.RestaurantName}\"\nCuisine: " +
+                $"{restaurant.Cuisine}\nRaiting: {restaurant.OverallRating}");
+        }
+        static void ShowReview(Review review)
+        {
+           // if (review = null)
+            Console.WriteLine($"Reviewed by {review.UserName}\nTaste: {review.StarsTaste}\nMood: " +
+                $"{review.StarsMood}\nService: {review.StarsService}\nPrice: {review.StarsPrice}\tNote: {review.Note}");
+        }
+        
+        static async Task<List<Restaurant>> GetAllRestaurantsAsync(string path)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            List <Restaurant> restaurant = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
             {
-                Console.WriteLine(repo.Name);
-                Console.WriteLine(repo.Description);
-                Console.WriteLine(repo.GitHubHomeUrl);
-                Console.WriteLine(repo.Homepage);
-                Console.WriteLine(repo.Watchers);
-                Console.WriteLine(repo.LastPush);
-                Console.WriteLine();
+                restaurant = await response.Content.ReadAsAsync<List<Restaurant>>();
             }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("It took "+ elapsedMs + "ms to read from DB");
+            return restaurant;
+        }
+        static void Main()
+        {
+            RunAsync().GetAwaiter().GetResult();
         }
 
-        private static async Task<List<Repository>> ProcessRepositories()
+        static async Task RunAsync()
         {
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+            client.BaseAddress = new Uri("http://localhost:7179/");
 
-            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
-            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
-            return repositories;
+            try
+            {
+                var url = "https://localhost:7179/All/Restaurants";
+
+                var restaurants = await GetAllRestaurantsAsync(url);
+                Console.WriteLine("\n---------List of all restaurants-----------");
+                foreach (var item in restaurants)
+                {
+                    Console.WriteLine("\n++++++++++++++++++++");
+                    ShowRestaurant(item);
+                    
+                    if (item.Reviews != null)
+                    {   
+                        foreach (var review in item.Reviews)
+                        {
+                            Console.WriteLine("--------------------");
+                            ShowReview(review);
+                            //Console.WriteLine("--------------------");
+                        }
+                    }
+                    else
+                        Console.WriteLine("No reviews yet");
+                        continue;
+                }
+                Console.WriteLine("\n--------------End of list------------------\n");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
